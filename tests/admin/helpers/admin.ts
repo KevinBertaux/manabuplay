91347484ebb5b09baa6afe1f3957264877e7c393
Kevin BERTAUX <58264753+KevinBertaux@users.noreply.github.com>
2026-04-25 16:13:54 +0200

    Tune local QA workflow

--- tests/admin/helpers/admin.ts
+++ tests/admin/helpers/admin.ts
@@ -1,2 +1,2 @@
-export const ADMIN_URL = "http://127.0.0.1:4322/";
-export const PUBLIC_URL = "http://127.0.0.1:4321/";
+export const ADMIN_URL = "http://127.0.0.1:4175/";
+export const PUBLIC_URL = "http://127.0.0.1:4176/";
